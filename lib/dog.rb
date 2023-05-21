312535fe478ad84f1b29da947750ea15f5a736b4
sunga-lee <sungajunior1@gmail.com>
2023-05-21 11:08:02 +0300

    completd assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
+    #Setter Method
+    def name=(name)
+        @name = name
+    end
 
+    #Getter Method
+    def name
+        @name
+    end
+
+    #Setter Method
+    def breed=(breed)
+        @breed = breed
+    end
+
+    #Getter Method
+    def breed
+        @breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,27 @@
 class Person
+     #Setter Method
+     def name=(name)
+        @name = name
+    end
+
+    #Getter Method
+    def name
+        @name
+    end
+
+    #Setter Method
+    def job=(job)
+        @job = job
+    end
+
+    #Getter Method
+    def job
+        @job
+    end
 
 end
+
+atara = Person.new
+atara.name = "Atara"
+
+puts atara.name
